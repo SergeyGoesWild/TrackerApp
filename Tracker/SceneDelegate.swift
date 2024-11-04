1850c13a35d2d9ccb950dfc8750963eb6467c5dd
Sergey Telnov <sergej.telnov@gmail.com>
2024-11-04 18:46:08 +0100

    Удалил файлы связанные со сторибордом

--- Tracker/SceneDelegate.swift
+++ Tracker/SceneDelegate.swift
@@ -17,6 +17,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        guard let window = window else { return }
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
