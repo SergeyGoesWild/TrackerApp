0b3b0a89f20c028372edf1a6b46873add67d104c
Sergey Telnov <sergej.telnov@gmail.com>
2024-11-05 13:31:34 +0100

    Сверстал экран трекеров, закончил онбординг урок

--- Tracker/TrackersViewController.swift
+++ Tracker/TrackersViewController.swift
@@ -11,18 +11,48 @@ import UIKit
 final class TrackersViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTrackerScreen()
+    }
+    
+    private func setupTrackerScreen() {
         view.backgroundColor = .white
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Trackers here!"
-        label.font =
-            .systemFont(ofSize: 20)
-        label.sizeToFit()
-        label.textAlignment = .center
-        view.addSubview(label)
+        
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Трекеры"
+        titleLabel.font = .systemFont(ofSize: 34, weight: .bold)
+        titleLabel.textColor = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 1.00)
+        titleLabel.textAlignment = .left
+        view.addSubview(titleLabel)
+        
+        let plusButton = UIButton()
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.setImage(UIImage(named: "PlusIcon.png"), for: .normal)
+        plusButton.setTitle("", for: .normal)
+        view.addSubview(plusButton)
+        
+        let starImage = UIImageView()
+        starImage.translatesAutoresizingMaskIntoConstraints = false
+        starImage.image = UIImage(named: "StarIcon")
+        view.addSubview(starImage)
+        
+        let starTextLabel = UILabel()
+        starTextLabel.translatesAutoresizingMaskIntoConstraints = false
+        starTextLabel.text = "Что будем отслеживать?"
+        starTextLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        starTextLabel.textColor = UIColor(red: 0.10, green: 0.11, blue: 0.13, alpha: 1.00)
+        starTextLabel.textAlignment = .center
+        view.addSubview(starTextLabel)
+        
         NSLayoutConstraint.activate([
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            plusButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 6),
+            plusButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 45),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 88),
+            starImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            starImage.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            starTextLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            starTextLabel.topAnchor.constraint(equalTo: starImage.bottomAnchor, constant: 8)
         ])
     }
 }
